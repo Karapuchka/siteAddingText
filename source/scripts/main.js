cf35546a8fbaf29c6382f4b00ad9e33f5f4d59f3
Karapuchka <karapu4ka2@gmail.com>
2022-08-30 23:35:07 +0300

    Добавил изменения при нажатию на кнопку "How  to use?"
    По нажатию на кнопку срабатывает событие, при котором меняется фон у всего body и элементов формы, отключаются кнопки, выделяется элемент формы о котором идет объяснение, меняется текст у кнопки на пояснение к элементу формы.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,9 +1,60 @@
-"use strict";
+"use strict"
+const body =  document.body;
 
-const formSearch = document.forms.formSearch
+const formSearch = document.forms.formSearch;
 
 formSearch.addEventListener('click', function(event){
     if(event.target.closest('.form-search__btn')){
         event.preventDefault();
     }
 })
+
+const instructionBtn = document.querySelector('.instruction-btn');
+const formSearchTextAreas = document.querySelectorAll('.form-search__textarea');
+const formSearchBtns = document.querySelectorAll('.form-search__btn');
+
+let countPart = 0;
+
+instructionBtn.addEventListener('click', function(event){
+    countPart++;
+
+    gsap.to([body, formSearchTextAreas[1], formSearchTextAreas[2], formSearchBtns], {duration: 2, background:'#D8D8D8'});
+
+    formSearchBtns.forEach(item =>{
+        item.style.cursor = 'default';
+        item.setAttribute('disabled', 'disabled');
+    });
+
+    formSearchTextAreas.forEach(item =>{
+        item.setAttribute('readonly', 'readonly');
+    });
+
+    if(countPart == 1){
+        gsap.to(instructionBtn, {textDecoration: 'none', color: '#0029FF'});
+
+        instructionBtn.innerText = 'In this field, you must enter the text in which the search will be performed.';
+    
+        instructionBtn.appendChild(createSpan());
+    }
+    
+    console.log(countPart);
+})
+
+function createSpan(){
+    let spanOk = document.createElement('span');
+    spanOk.innerText = 'ok';
+    spanOk.style.textTransform = 'uppercase';
+    spanOk.style.color = '#71AAFF';
+    spanOk.style.textDecoration = 'underline';
+    spanOk.style.margin = '0 0 0 10px';
+
+    spanOk.addEventListener('mouseout', function(){
+        gsap.to(spanOk, {duration: 1, color: '#71AAFF'})
+    });
+
+    spanOk.addEventListener('mouseover', function(){
+        gsap.to(spanOk, {duration: 1, color: '#19478c'})
+    });
+
+    return spanOk;
+}
