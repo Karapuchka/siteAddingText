6d1dc6a7659112302f4afc1d05131b5b3da88c5a
Karapuchka <karapu4ka2@gmail.com>
2022-12-17 21:47:56 +0300

    Свертал макет

--- gulp/source/scripts/main.js
+++ gulp/source/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict'
 
+gsap.to('.user-icon', {
+    background: `url(${localStorage.getItem('userIcon')}) no-repeat center center`,
+    backgroundSize: '100%',
+    opacity: 1,
+    duration: .8,
+});
+
+gsap.to('.greetings', {
+    delay: .6,
+    opacity: 1,
+    duration: .7,
+    y: -70,
+});
+
+setTimeout(()=>{
+    gsap.to('.preloader', {
+        opacity: 0,
+        duration: .5,
+    });
+
+    gsap.to('.preloader', {
+        delay: .8,
+        display: 'none',
+    });
+}, 2500)
+
 let detect = new MobileDetect(window.navigator.userAgent)
 
-console.log(detect.userAgent());
+// Анимация для мобильной и ПК версиях
+if(detect.mobile()){
+
+} else {
+
+}
